6baddd00cf884cd613bd1aa086d381c69ad337fb
martinvanelp <martin.vanelp@gmail.com>
2015-11-22 18:14:19 +0100

    second upload of PML assignment

--- Practical_Machine_Learning/PML_project/exercise.R
+++ Practical_Machine_Learning/PML_project/exercise.R
@@ -71,3 +71,16 @@ set.seed(1234)
 rfFit <- train(classe ~ ., data = exercise, method = "rf")
 rfFit$finalModel
 
+# Predictions
+rfTestPred <- predict(rfFit, testing)
+answers <- rfTestPred
+
+pml_write_files = function(x){
+        n = length(x)
+        for(i in 1:n){
+                filename = paste0("problem_id_",i,".txt")
+                write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+        }
+}
+
+pml_write_files(answers)
